2abed35cf5338e8199861e81b8b1f7cb0a69c9c2
Piotr Marecki <pmarecki.pro@gmail.com>
2016-06-03 18:36:12 +0200

    zajecia1

---
+++ src/main/java/W.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by mareckip on 03.06.16.
+ */
+public class W {
+    public static void main(String[] args) throws Exception {
+            InetAddress addr = InetAddress.getByName("10.11.12.13");
+            ServerSocket ss = new ServerSocket(22222, 50, addr);
+            Socket s = ss.accept();
+            System.out.println("połączenie nawiązano...");
+            InputStream is = s.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = "";
+            while((line=br.readLine())!=null) {
+                if ("ex".equals(line)) break;
+                System.out.println("-> " + line);
+            }
+            s.close();
+    }
+}
